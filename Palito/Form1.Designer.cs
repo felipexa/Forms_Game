46c6814764deaf09f80304474d4ea409097ea07a
Felipe Xavier <felipexavierass@gmail.com>
2020-07-12 22:00:20 -0300

    palito

---
+++ Palito/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Palito
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.buttonretirar = new System.Windows.Forms.Button();
+            this.buttonjogar = new System.Windows.Forms.Button();
+            this.nupdquantidade = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nupdquantidade)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(309, 72);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(243, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Quantidade de palitos  deseja retirar:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(570, 67);
+            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.maskedTextBox1.Mask = "0";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(89, 22);
+            this.maskedTextBox1.TabIndex = 11;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            this.maskedTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.maskedTextBox1_KeyDown);
+            // 
+            // buttonretirar
+            // 
+            this.buttonretirar.Location = new System.Drawing.Point(677, 61);
+            this.buttonretirar.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonretirar.Name = "buttonretirar";
+            this.buttonretirar.Size = new System.Drawing.Size(100, 28);
+            this.buttonretirar.TabIndex = 10;
+            this.buttonretirar.Text = "Retirar";
+            this.buttonretirar.UseVisualStyleBackColor = true;
+            this.buttonretirar.Click += new System.EventHandler(this.buttonretirar_Click);
+            // 
+            // buttonjogar
+            // 
+            this.buttonjogar.Location = new System.Drawing.Point(677, 22);
+            this.buttonjogar.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonjogar.Name = "buttonjogar";
+            this.buttonjogar.Size = new System.Drawing.Size(100, 28);
+            this.buttonjogar.TabIndex = 9;
+            this.buttonjogar.Text = "Jogar";
+            this.buttonjogar.UseVisualStyleBackColor = true;
+            this.buttonjogar.Click += new System.EventHandler(this.buttonjogar_Click);
+            this.buttonjogar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.buttonjogar_KeyDown);
+            // 
+            // nupdquantidade
+            // 
+            this.nupdquantidade.Location = new System.Drawing.Point(570, 28);
+            this.nupdquantidade.Margin = new System.Windows.Forms.Padding(4);
+            this.nupdquantidade.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nupdquantidade.Minimum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nupdquantidade.Name = "nupdquantidade";
+            this.nupdquantidade.Size = new System.Drawing.Size(89, 22);
+            this.nupdquantidade.TabIndex = 8;
+            this.nupdquantidade.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nupdquantidade.ValueChanged += new System.EventHandler(this.nupdquantidade_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(309, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Escolha a quantidade de Palitos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 107);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1033, 445);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1115, 578);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.buttonretirar);
+            this.Controls.Add(this.buttonjogar);
+            this.Controls.Add(this.nupdquantidade);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nupdquantidade)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button buttonretirar;
+        private System.Windows.Forms.Button buttonjogar;
+        private System.Windows.Forms.NumericUpDown nupdquantidade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Palito/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Palito
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Inicia o jogo com label, botao e masked box da quantidade de palitos que deseja retirar invisiveis
+
+            label2.Visible = false;
+            buttonretirar.Visible = false;
+            maskedTextBox1.Visible = false;
+            this.Text = "Jogo do Palito"; // titulo do form
+        }
+
+        int x;
+        bool jogada = false;
+       
+
+        private void buttonjogar_Click(object sender, EventArgs e)
+        {
+            int valor1 = (int)nupdquantidade.Value;
+            if (valor1 == 30 || valor1 == 26 || valor1 == 22) // refere-se a quantidade a ser removida que vai ser sempre 1
+            {
+                richTextBox1.AppendText("Eu começo!" + Environment.NewLine); //quantidade de palitos que iniciou
+                richTextBox1.AppendText("Retiro 1 Palito" + Environment.NewLine); // quantidade de palitos que foi retirado
+                richTextBox1.AppendText("Sua Vez..." + Environment.NewLine); // sua vez de jogar
+                x = (valor1 - 1); // valor1 menos 1
+                richTextBox1.AppendText("Restam " + x.ToString() + " Palitos" + Environment.NewLine); // quantidade de palitos que sobraram
+                jogada = false;
+            }
+
+            if (valor1 == 29 || valor1 == 28 || valor1 == 25 || valor1 == 24 || valor1 == 21 || valor1 == 20)// refere-se a quantidade a ser removida que vai ser sempre 3
+            {
+                richTextBox1.AppendText("Eu começo!" + Environment.NewLine); //quantidade de palitos que iniciou
+                richTextBox1.AppendText("Retiro 3 Palitos" + Environment.NewLine); // quantidade de palitos que foi retirado pelo computador
+                richTextBox1.AppendText("Sua Vez..." + Environment.NewLine);// sua vez de jogar
+                x = (valor1 - 3); // valor1 referente ao if menos 3 
+                richTextBox1.AppendText("Restam " + x.ToString() + " Palitos" + Environment.NewLine);// quantidade de palitos que sobraram
+                jogada = false;
+            }
+
+            if (valor1 == 27 || valor1 == 23)// refere-se a quantidade a ser removida que vai ser sempre 2
+            {
+                richTextBox1.AppendText("Eu começo!" + Environment.NewLine); //quantidade de palitos que iniciou
+                richTextBox1.AppendText("Retiro 2 Palito" + Environment.NewLine); // quantidade de palitos que foi retirado pelo computador
+                richTextBox1.AppendText("Sua Vez..." + Environment.NewLine); // sua vez de jogar
+                x = (valor1 - 2); // efetua  o menos um referente  ai if //
+                richTextBox1.AppendText("Restam " + x.ToString() + " Palitos" + Environment.NewLine);// quantidade de palitos que sobraram
+                jogada = false;
+                // remove de 4 em 4  29/25/21/17/13/9/5***************//
+            }
+
+            // Assim que clicar em jogar ele ativa os 3 componentes para retirar os palitos e desabilita o botao jogar da quantidade de palitos
+            label2.Visible = true;
+            buttonretirar.Visible = true;
+            maskedTextBox1.Visible = true;
+            buttonjogar.Enabled = false;
+            maskedTextBox1.Focus(); //cursor voltado para o jogador digitar quantidade que deseja retirar
+
+        }
+
+        private void buttonretirar_Click(object sender, EventArgs e)
+        {
+
+
+            //se o campo que o jogador vai inserir a quantidade de palitos a retirar estiver vazio
+            if (maskedTextBox1.Text == "".ToString())
+            {
+                //aparecera uma mensagem informando que o preenchimento e obrigatorio
+                MessageBox.Show("Preenchimento obrigatorio da quantidade de palitos a remover.");
+            }
+            //caso contrario executa todas as acoes do codigo
+            else
+            {
+                int retira = Convert.ToInt32(maskedTextBox1.Text); //convertendo para inteiro pois maskedtextbox recebe texto
+
+
+
+
+
+                if (retira != 1 && retira != 2 && retira != 3) // verificar se  o numero digitado e diferente de 1, 2 ou 3
+                {
+                    // mensagem informando ao usuario caso o numero foi diferente do if, vai informar que nao aceita o valor
+                    // maskedTextBox1.Focus() = cursor voltado para o jogador digitar quantidade que deseja retirar
+                    MessageBox.Show("São permitidos retirar 1 a 3 palitos por jogada", "Error" + MessageBoxIcon.Error + maskedTextBox1.Focus());
+                    maskedTextBox1.Clear(); //limpar campo
+                }
+
+
+                else
+                {
+                    {
+                        jogada = true;
+                        // se o x for igual a 5 e a jogada for verdadeira
+                        if (x == 5 && jogada == true)
+                        {
+                            // 5 menos o valor inserido pelo jogador (1,2 ou 3)
+                            x = x - retira;
+                            richTextBox1.AppendText("Voce retirou " + retira + " Palitos"); //quantidade de palitos removidos
+                            richTextBox1.AppendText("Restam " + x + " Palitos"); //quantidade de palitos que restaram
+
+                            //se o x for igual a 4
+                            if (x == 4)
+                            {
+                                // 4 menos 3
+                                x = x - 3;
+                                richTextBox1.AppendText("Retiro 3 palitos" + Environment.NewLine); //quantidade de palitos removidos
+                                richTextBox1.AppendText("Sua vez..." + Environment.NewLine); // mensagem informando que e a sua vez de jogar
+                                richTextBox1.AppendText("Restam " + x + " Palitos" + Environment.NewLine); //quantidade de palitos que restaram
+
+                            }
+
+                            //se o x for igual a 3
+                            if (x == 3)
+                            {
+                                // 3 menos 2
+                                x = x - 2;
+                                richTextBox1.AppendText("Retiro 2 palitos" + Environment.NewLine); //quantidade de palitos removidos
+                                richTextBox1.AppendText("Sua vez..." + Environment.NewLine); // mensagem informando que e a sua vez de jogar
+                                richTextBox1.AppendText("Restam " + x + " Palitos" + Environment.NewLine); //quantidade de palitos que restaram
+                            }
+
+                            //se o x for igual a 2
+                            if (x == 2)
+                            {
+                                //2 menos 3
+                                x = x - 3;
+                                richTextBox1.AppendText("Retiro 1 palito" + Environment.NewLine); //quantidade de palitos removidos
+                                richTextBox1.AppendText("Sua vez..." + Environment.NewLine); // mensagem informando que e a sua vez de jogar
+                                richTextBox1.AppendText("Restam " + x + " Palitos" + Environment.NewLine); //quantidade de palitos que restaram
+                            }
+
+                            //se o x for igual a 1
+                            if (x == 1)
+                            {
+                                //1 menos 1
+                                x = x - 1;
+                                MessageBox.Show("Voce perdeu...", "kkkk muito facil!"); //mensagem informando que voce perdeu o jogo
+                                // quando finalizar o jogo aparece uma mensagem perguntando ao jogador se deseja jogar novamente com opcao de sim ou nao
+                                DialogResult dialogResult = MessageBox.Show("Deseja jogar de novo", "Desafio", MessageBoxButtons.YesNo);
+
+                                // se o jogador clicar em sim
+                                if
+                                    (dialogResult == DialogResult.Yes)
+                                {
+                                    // limpa tudo e inicia o jogo
+                                    richTextBox1.Clear();
+                                    label2.Visible = false;
+                                    buttonretirar.Visible = false;
+                                    maskedTextBox1.Visible = false;
+                                    buttonjogar.Enabled = true;
+                                }
+
+                                // se o usuario clicar em nao
+                                else if (dialogResult == DialogResult.No)
+                                {
+                                    Environment.Exit(1); //finaliza o jogo // (0) verdadeiro = deixa o jogo rodando;  (1) falso = fecha o jogo
+                                }
+                            }
+                        }
+                        else
+                        {
+                            richTextBox1.AppendText("Você Retirou " + retira + " Palitos" + Environment.NewLine); // quantidade de palitos removidos
+                            x -= retira; // subtrair a quantidade retirada
+                            richTextBox1.AppendText("Restam " + x + " Palitos" + Environment.NewLine); // quantidade de palitos que sobraram
+                            maskedTextBox1.Clear(); // limpa o campo para inserir um novo numero para ser removido
+
+                            if (retira == 2)
+                            {
+                                jogada = false;
+                                x = x - 2; //logica de 4 em 4 **** 2 mais 2 =4 //
+                                richTextBox1.AppendText("Retiro 2 palitos" + Environment.NewLine); // quantidade de palitos removidos
+                                richTextBox1.AppendText("Sua vez... " + Environment.NewLine); // mensagem informando que e a sua vez de jogar
+                                richTextBox1.AppendText("Restam " + x + " Palitos " + Environment.NewLine); // quantidade de palitos que restaram
+                            }
+
+                            if (retira == 3)
+                            {
+                                jogada = false;
+                                x = x - 1;  //logica de 4 em 4 **** 3 mais 1 =4 //
+                                richTextBox1.AppendText("Retiro 3 palitos" + Environment.NewLine); // quantidade de palitos removidos
+                                richTextBox1.AppendText("Sua vez... " + Environment.NewLine); // mensagem informando que e a sua vez de jogar
+                                richTextBox1.AppendText("Restam " + x + " Palitos " + Environment.NewLine); // quantidade de palitos que restaram
+                            }
+                            if (retira == 1)
+                            {
+                                jogada = false;
+                                x = x - 3;//logica de 4 em 4 **** 1 mais 3 =4 //
+                                richTextBox1.AppendText("Retiro 3 palitos " + Environment.NewLine); // quantidade de palitos removidos
+                                richTextBox1.AppendText("Sua vez..." + Environment.NewLine); // mensagem informando que e a sua vez de jogar
+                                richTextBox1.AppendText("Restam " + x + " Palitos" + Environment.NewLine); // quantidade de palitos que restaram
+                            }
+                            maskedTextBox1.Focus(); //cursor voltado para o jogador digitar quantidade que deseja retirar
+                            
+                            //se x for menor que 1
+                            if(x<1)
+                            {
+                                //aparece uma mensagem dizendo que a maquina perdeu, ou seja, o jogador venceu!
+                                MessageBox.Show("Eu perdi", "Impossivel");
+                                // quando finalizar o jogo aparece uma mensagem perguntando ao jogador se deseja jogar novamente com opcao de sim ou nao
+                                DialogResult dialogResult = MessageBox.Show("Deseja jogar de novo", "Desafio", MessageBoxButtons.YesNo);
+
+                                // se o jogador clicar em sim
+                                if
+                                    (dialogResult == DialogResult.Yes)
+                                {
+                                    // limpa tudo e inicia o jogo
+                                    richTextBox1.Clear();
+                                    label2.Visible = false;
+                                    buttonretirar.Visible = false;
+                                    maskedTextBox1.Visible = false;
+                                    buttonjogar.Enabled = true;
+                                }
+
+                                // se o usuario clicar em nao
+                                else if (dialogResult == DialogResult.No)
+                                {
+                                    Environment.Exit(1); //finaliza o jogo // (0) verdadeiro = deixa o jogo rodando;  (1) falso = fecha o jogo
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nupdquantidade_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void buttonjogar_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+    }
+}
